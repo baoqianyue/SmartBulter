c4724e19bb754a12e3b0754ef783de5b9a37eb75
stbtkl <987132349@qq.com>
2017-06-01 13:03:53 +0800

    package sharedPreferences

---
+++ app/src/main/java/com/barackbao/smartbutler/utils/ShareUtils.java
@@ -0,0 +1,58 @@
+package com.barackbao.smartbutler.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by BarackBao on 2017/6/1.
+ * 封装SharedPreferences
+ * 三种数据类的getter，setter
+ * 删除一个和全部
+ */
+
+public class ShareUtils {
+    private static final String NAME = "config";
+
+
+    public static void putString(Context mContext, String key, String value) {
+        SharedPreferences sp = mContext.getSharedPreferences(NAME, Context.MODE_PRIVATE);
+        sp.edit().putString(key, value).commit();
+    }
+
+    public static String getString(Context mContext, String key, String defValue) {
+        SharedPreferences sp = mContext.getSharedPreferences(NAME, Context.MODE_PRIVATE);
+        return sp.getString(key, defValue);
+    }
+
+    public static void putInt(Context mContext, String key, int value) {
+        SharedPreferences sp = mContext.getSharedPreferences(NAME, Context.MODE_PRIVATE);
+        sp.edit().putInt(key, value).commit();
+    }
+
+    public static int getInt(Context mContext, String key, int defValue) {
+        SharedPreferences sp = mContext.getSharedPreferences(NAME, Context.MODE_PRIVATE);
+        return sp.getInt(key, defValue);
+    }
+
+    public static void putBoolean(Context mContext, String key, Boolean value) {
+        SharedPreferences sp = mContext.getSharedPreferences(NAME, Context.MODE_PRIVATE);
+        sp.edit().putBoolean(key, value).commit();
+    }
+
+    public static Boolean getString(Context mContext, String key, Boolean defValue) {
+        SharedPreferences sp = mContext.getSharedPreferences(NAME, Context.MODE_PRIVATE);
+        return sp.getBoolean(key, defValue);
+    }
+
+    //删除单个
+    public static void deleShare(Context mContext, String key) {
+        SharedPreferences sp = mContext.getSharedPreferences(NAME, Context.MODE_PRIVATE);
+        sp.edit().remove(key).commit();
+    }
+
+    //删除全部
+    public static void deleAll(Context mContext) {
+        SharedPreferences sp = mContext.getSharedPreferences(NAME, Context.MODE_PRIVATE);
+        sp.edit().clear().commit();
+    }
+}
